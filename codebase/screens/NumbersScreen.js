dc5e31e38db9060dceb3e7bf271497cd25ffcb6a
SIMS-B <22100128@lums.edu.pk>
2022-07-16 09:40:42 +0500

    Fully working basic app code [updated and FNF]

--- codebase/screens/NumbersScreen.js
+++ codebase/screens/NumbersScreen.js
@@ -1,11 +1,17 @@
 // component imports
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 
 // color imports
-import colors from "../codebase/config/colors";
+import colors from "../config/colors";
 
 // screen layout
 export default function Home(props) {
+  // contact numbers' db
+  db = [
+    { name: "Syed Shafaat Hussain", number: "0333 9606914", key: "1" },
+    { name: "SIMS B.", number: "0336 9519321", key: "1" },
+  ];
+
   return (
     <View style={styles.container}>
       <Text> Numbers Screen is here </Text>
